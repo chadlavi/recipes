344edf5efe7113dab18072b756f766599d0cef64
Chad Lavimoniere <chad@chadlavimoniere.com>
2022-11-29 20:27:49 -0500

    adjust indexer markdown

--- food/mains/barbacoa-tacos.md
+++ food/mains/barbacoa-tacos.md
@@ -1,21 +1,23 @@
-# Tacos de barbacoa de borrego 
+# Tacos de barbacoa de borrego
 
 Makes 16 to 20 tacos.
 
 ## Ingredients
+
 - 2 dried ancho chiles (about 1 ounce), wiped clean, stemmed, seeded, and deveined
 - 2 dried guajillo chiles (about Y2 ounce), wiped clean, stemmed, slit open, seeded, and deveined
 - 4 dried arbol chiles (about 1 ounce), wiped clean and stemmed (optional)
 - 2 tablespoons apple cider vinegar, or more to taste
 - 5 large garlic cloves, peeled
 - 1 teaspoon cumin seeds
-- 1 teaspoon dried Mexican oregano 
-- 4 whole cloves 
+- 1 teaspoon dried Mexican oregano
+- 4 whole cloves
 - 2 teaspoons kosher salt
-- 1 (2.5-pound) boneless lamb shoulder roast 
+- 1 (2.5-pound) boneless lamb shoulder roast
 
 ## Directions
-1. Preheat the oven to 350°F. 
+
+1. Preheat the oven to 350°F.
 2. Preheat a large pan over medium-low heat. Toast the ancho, guajillo, and arbol chiles, turning them over and pressing down on them with tongs frequently, until the anchos and guajillos are fragrant, about 1 to 1 1/2 minutes, and the arbol chiles have browned all over with some dark spots, 3 to 4 minutes.
 3. Soak the chiles in enough cold water to cover until they're soft, about 20 minutes. Drain, discarding the water.
 4. Blend the chiles with the vinegar, the garlic, cumin, oregano, cloves, salt, and 1/2 cup of fresh water in a blender to form a very smooth puree, at least 3 minutes.
@@ -24,7 +26,8 @@ Makes 16 to 20 tacos.
 
 *Do ahead: The saucy shredded lamb keeps in the fridge for up to three days or in the freezer for up to one month.*
 
-Serve alongside 16 to 20 warm corn tortillas and lime wedges and top with chopped white onion, chopped cilantro, and fried chile salsa or pico de gallo with lemon zest. 
+Serve alongside 16 to 20 warm corn tortillas and lime wedges and top with chopped white onion, chopped cilantro, and fried chile salsa or pico de gallo with lemon zest.
 
 ## Reference
-* *Tacos, Tortas And Tamales*, by Roberto Santibañez
+
+- *Tacos, Tortas And Tamales*, by Roberto Santibañez
--- utils/indexer.sh
+++ utils/indexer.sh
@@ -2,19 +2,19 @@
 
 start="# Recipes
 
-It’s recipes. You know, to cook with. 
+It’s recipes. You know, to cook with.
 
 <https://github.com/chadlavi/recipes>
 
 ## Categories
+
 [Breads](#breads) &middot; [Breakfasts](#breakfasts) &middot; [Desserts](#desserts) &middot; [Main courses](#main-courses) &middot; [Pasta](#pasta) &middot; [Pizza](#pizza) &middot; [Salads](#salads) &middot; [Sandwiches](#sandwiches) &middot; [Sides and dips](#sides-and-dips) &middot; [Snacks](#snacks) &middot; [Soup](#soup) &middot; [Cured and pickled](#cured-and-pickled) &middot; [Condiments, sauces and toppings](#condiments-sauces-and-toppings) &middot; [Cocktails](#cocktails) &middot; [Coffee and tea](#coffee-and-tea) &middot; [Brewing](#brewing)
 
 ## Contents"
 
 end="🍇🍈🍉🍊🍋🍌🍍🍎🍏🍐🍑🍒🍓🥝🍅🥑🍆🥔🥕🌽🌶🥒🍄🥜🌰🍞🥐🥖🥞🧀🍖🍗🥓🍔🍟🍕🌭🌮🌯🍳🍲🥗🍿🍱🍘🍙🍚🍛🍜🍝🍠🍢🍣🍤🍥🍡🍦🍧🍨🍩🍪🎂🍰🍫🍬🍭🍮🍯🍼🥛☕🍵🍶🍾🍷🍸🍹🍺🍻🥂🥃🍽🍴🥄
 
-<i>Inspired by <https://github.com/shaniber/recipes></i>
-"
+_Inspired by <https://github.com/shaniber/recipes>_"
 
 getMarkdownLinks() {
   for file in $(find . -iname "*.md" -not -name "README.md" -not -iname "*template*"); do
@@ -33,53 +33,67 @@ foodLinks="$(filter "$markdownLinks" food | sort)"
 echo "$start
 
 ### Breads
-<details>
+
 $(filter "$foodLinks" "/bread")
-</details>
 
 ### Breakfasts
+
 $(filter "$foodLinks" breakfast)
 
 ### Desserts
+
 $(filter "$foodLinks" desserts)
 
 ### Main courses
+
 $(filter "$foodLinks" mains)
 
 ### Pasta
+
 $(filter "$foodLinks" pasta)
 
 ### Pizza
+
 $(filter "$foodLinks" pizza)
 
 ### Salads
+
 $(filter "$foodLinks" salads)
 
 ### Sandwiches
+
 $(filter "$foodLinks" sandwiches)
 
 ### Snacks
+
 $(filter "$foodLinks" snacks)
 
 ### Soup
+
 $(filter "$foodLinks" soup)
 
 ### Cured and pickled
+
 $(filter "$foodLinks" cured-pickled)
 
 ### Sides and dips
+
 $(filter "$foodLinks" sides-dips)
 
 ### Condiments, sauces and toppings
+
 $(filter "$foodLinks" condiments-sauces-toppings)
 
 ### Cocktails
+
 $(filter "$drinkLinks" cocktails)
 
 ### Coffee and Tea
+
 $(filter "$drinkLinks" coffee\|tea)
 
 ### Brewing
+
 $(filter "$drinkLinks" beers)
 
 $end"
