8ff2003c35631c64d9453efcafb27be9d37d6e09
Chad Lavimoniere <chad@chadlavimoniere.com>
2023-10-14 19:05:41 -0400

    add Zojirushi reference

--- _utils/indexer.sh
+++ _utils/indexer.sh
@@ -10,6 +10,8 @@ It's recipes. You know, to cook with.
 
 [Breads](#breads) &middot; [Breakfasts](#breakfasts) &middot; [Desserts](#desserts) &middot; [Main courses](#main-courses) &middot; [Pasta](#pasta) &middot; [Pizza](#pizza) &middot; [Salads](#salads) &middot; [Sandwiches](#sandwiches) &middot; [Sides and dips](#sides-and-dips) &middot; [Snacks](#snacks) &middot; [Soup](#soup) &middot; [Cured and pickled](#cured-and-pickled) &middot; [Condiments, sauces and toppings](#condiments-sauces-and-toppings) &middot; [Cocktails](#cocktails) &middot; [Coffee and tea](#coffee-and-tea) &middot; [Brewing](#brewing) &middot; [Holiday](#holiday) &middot; [Pressure cooker](#pressure-cooker)
 
+[Rice cooker reference](./food/rice/zojirushi-reference.md)
+
 ## Contents"
 
 end="🍇🍈🍉🍊🍋🍌🍍🍎🍏🍐🍑🍒🍓🥝🍅🥑🍆🥔🥕🌽🌶🥒🍄🥜🌰🍞🥐🥖🥞🧀🍖🍗🥓🍔🍟🍕🌭🌮🌯🍳🍲🥗🍿🍱🍘🍙🍚🍛🍜🍝🍠🍢🍣🍤🍥🍡🍦🍧🍨🍩🍪🎂🍰🍫🍬🍭🍮🍯🍼🥛☕🍵🍶🍾🍷🍸🍹🍺🍻🥂🥃🍽🍴🥄
---
+++ food/rice/zojirushi-reference.md
@@ -0,0 +1,21 @@
+# Zojirushi rice cooker reference
+
+Note that this reference is specific to the Zojirushi NS-TSC10 model. See <http://www.zojirushi.com/grains> for other models.
+
+| Grain | Water per 1 cup | Setting |
+| --- | --- | --- |
+| Short / Medium Grain White Rice | use marked lines in bowl | `white` |
+| Short / Medium Grain Brown Rice | use marked lines in bowl | `brown` |
+| Sweet Rice | use marked lines in bowl | `sweet` |
+| Long Grain White Rice | 1.25 | `mixed` |
+| Jasmine Rice (White) | 1.25 | `white` |
+| Basmati Rice (White) | 1.5 | `white` |
+| Wild Rice | 1.5 | `brown` |
+| Quinoa | 1.25 | `quick` |
+| Rolled Oats | 1.5 | `white` |
+| Steel Cut Oats | 2.5 | `porridge` |
+
+## Reference
+
+- <https://www.zojirushi.com/grains/nszcc10.html>
+- <http://archive.today/nEhZ8> (Archive link)
